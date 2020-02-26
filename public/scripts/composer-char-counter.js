c51ec868cd32bab9ebc06c4b957f764353e0a14a
sruthikorad36 <sruthikorada36@gmail.com>
2020-02-26 01:26:54 +0000

    creating the tweet with html and css

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,10 +1,11 @@
 $(document).ready(function() {
     // --- our code goes here ---
     $(".new-tweet textarea").keyup(function(){
-        var value = $(this).val();
+        //console logging this within your callback function
+        var value = $(this).val(); 
         var count = value.length;
         var counter = $(this).closest('.new-tweet').find('.counter')
-        counter.text(140-count);
-        counter.css('color',(count > 140) ? "red" : "black" )
-    });
+        counter.text(140-count);// reduce the count when we type text
+        counter.css('color',(count > 140) ? "red" : "black" ) // condition stating that if count 
+    });                                                       //is more that 140 turn into negative and red
   });
