86dd844577296a5c25c5ef0d6ddcc1991947a08e
sruthikorad36 <sruthikorada36@gmail.com>
2020-02-27 23:41:17 +0000

    making static html and css to responsive designing

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -2,50 +2,26 @@
  * Client-side JS logic goes here
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
- */
-// Fake data taken from initial-tweets.json
-// const data = [
-//     {
-//       "user": {
-//         "name": "Newton",
-//         "avatars": "https://i.imgur.com/73hZDYK.png"
-//         ,
-//         "handle": "@SirIsaac"
-//       },
-//       "content": {
-//         "text": "If I have seen further it is by standing on the shoulders of giants"
-//       },
-//       "created_at": 1461116232227
-//     },
-//     {
-//       "user": {
-//         "name": "Descartes",
-//         "avatars": "https://i.imgur.com/nlhLi3I.png",
-//         "handle": "@rd" },
-//       "content": {
-//         "text": "Je pense , donc je suis"
-//       },
-//       "created_at": 1461113959088
-//     }
-//   ]
+*/
+// for avoiding hacking "cross-site scripting "
 const escape =  function(str) {
   let div = document.createElement('div');
   div.appendChild(document.createTextNode(str));
   return div.innerHTML;
-}
+};
+// loops through tweets
+  // calls createTweetElement for each tweet
+  // takes return value and appends it to the tweets container
+const renderTweets = function(tweets) {  
+  $('.tweets-container').empty();
+  for (let tweet of tweets) {
+    let $tweet = createTweetElement(tweet);
+    $(document).ready(function() {
+      $('.tweets-container').prepend($tweet);
+    });
+  }
+};
 
-  const renderTweets = function(tweets) {
-    // loops through tweets
-    // calls createTweetElement for each tweet
-    // takes return value and appends it to the tweets container
-    $('.tweets-container').empty();
-    for (let tweet of tweets) {
-      let $tweet =createTweetElement(tweet);
-      $(document).ready(function() {
-        $('.tweets-container').prepend($tweet);
-      });
-    }
-  };
 const createTweetElement = function(tweet) {
   let $tweet = $(`<article class=“tweet”>
   <header>	
@@ -71,62 +47,64 @@ const createTweetElement = function(tweet) {
 </article>`).addClass('tweet');
   // ...
   return $tweet;
-}
- //function for load all tweets.
+};
+
+//function for load all tweets.
 function loadTweets() {
   $.ajax({
     url: '/tweets',
     method: 'GET',
     dataType: "json",
-    success: function (tweetsArray) {
+    success: function(tweetsArray) {
       //console.log('Success POST : ', tweetsArray);
       renderTweets(tweetsArray);
     }
   });
   
 }
- loadTweets();
+loadTweets();
+
 $(document).ready(function() {
   $(function() {
     //get the form.
-   const $form = $('#submit');
+    const $form = $('#submit');
     //get the session of article.
     const $tweets = $('.container-tweets');
     const $textarea = $('#tweet-area');
     let $errSpan = $('.err-message');
     let $errSht = $('.shrterr');
     // Set up an event listener for the contact form.
-    $form.submit( function(event) {
-       $errSpan.slideUp(300);
-       $errSht.slideUp(300);
+    $form.submit(function(event) {
+      $errSpan.slideUp(300);
+      $errSht.slideUp(300);
       // Stop the browser from submitting the form.
       event.preventDefault();
-      // if tweent is empty or more than 140 just display span with proper msg for 1500ms time.
-      var text = $textarea.val();
-      if(text === '' || text === null) {
+      // if tweent is empty or more than 140 get an error message.
+      let text = $textarea.val();
+      if (text === '' || text === null) {
         $errSht.text("Say sumthing guyssss !!!!!!")
-        .slideToggle(400);
+          .slideToggle(400);
 
-      } else if(text.length > 140) {
+      } else if (text.length > 140) {
         $errSpan.text("Tweet length more than 140 characters")
-        .slideToggle(400);
+          .slideToggle(400);
       } else {
         // Submit the form using AJAX.
         $.ajax({
-            type: 'POST',
-            url: '/tweets',
-            data: $form.serialize(),
-            success: function() {
-              loadTweets();
-            }
-        })
+          type: 'POST',
+          url: '/tweets',
+          data: $form.serialize(),
+          success: function() {
+            loadTweets();
+          }
+        });
         //empty the tweet textarea after tweet submits the post.
         $textarea.val("");
       }
     });
   });
   //when we click compose button it slideDown newtweet and with the return value of slideToggle
-    // we are changing the toggle button appearance.
+  // we are changing the toggle button appearance.
   $(".toggle-btn").click(() => {
     $(".new-tweet").slideToggle("slow");
     $(".new-tweet textarea").focus();
--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,11 +1,11 @@
 $(document).ready(function() {
-    // --- our code goes here ---
-    $(".new-tweet textarea").keyup(function(){
-        //console logging this within your callback function
-        var value = $(this).val(); 
-        var count = value.length;
-        var counter = $(this).closest('.new-tweet').find('.counter')
-        counter.text(140-count);// reduce the count when we type text
-        counter.css('color',(count > 140) ? "red" : "black" ) // condition stating that if count 
-    });                                                       //is more that 140 turn into negative and red
-  });
+  // --- our code goes here ---
+  $(".new-tweet textarea").keyup(function() {
+    //console logging this within your callback function
+    let value = $(this).val();
+    let count = value.length;
+    let counter = $(this).closest('.new-tweet').find('.counter');
+    counter.text(140 - count);// reduce the count when we type text
+    counter.css('color',(count > 140) ? "red" : "black"); // condition stating that if count
+  });                                                       //is more that 140 turn into negative and red
+});
