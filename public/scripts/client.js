5c8fc781be8b4d48c4ff1db6c203dbdb5e37c257
sruthikorad36 <sruthikorada36@gmail.com>
2020-02-27 19:35:52 +0000

    adding errors slide down to write a new tweet

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,30 +4,35 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 // Fake data taken from initial-tweets.json
-const data = [
-    {
-      "user": {
-        "name": "Newton",
-        "avatars": "https://i.imgur.com/73hZDYK.png"
-        ,
-        "handle": "@SirIsaac"
-      },
-      "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
-      "created_at": 1461116232227
-    },
-    {
-      "user": {
-        "name": "Descartes",
-        "avatars": "https://i.imgur.com/nlhLi3I.png",
-        "handle": "@rd" },
-      "content": {
-        "text": "Je pense , donc je suis"
-      },
-      "created_at": 1461113959088
-    }
-  ]
+// const data = [
+//     {
+//       "user": {
+//         "name": "Newton",
+//         "avatars": "https://i.imgur.com/73hZDYK.png"
+//         ,
+//         "handle": "@SirIsaac"
+//       },
+//       "content": {
+//         "text": "If I have seen further it is by standing on the shoulders of giants"
+//       },
+//       "created_at": 1461116232227
+//     },
+//     {
+//       "user": {
+//         "name": "Descartes",
+//         "avatars": "https://i.imgur.com/nlhLi3I.png",
+//         "handle": "@rd" },
+//       "content": {
+//         "text": "Je pense , donc je suis"
+//       },
+//       "created_at": 1461113959088
+//     }
+//   ]
+const escape =  function(str) {
+  let div = document.createElement('div');
+  div.appendChild(document.createTextNode(str));
+  return div.innerHTML;
+}
 
   const renderTweets = function(tweets) {
     // loops through tweets
@@ -36,7 +41,7 @@ const data = [
     for (let tweet of tweets) {
       let $tweet =createTweetElement(tweet);
       $(document).ready(function() {
-        $('.container').append($tweet);
+        $('.tweets-container').prepend($tweet);
       });
     }
   };
@@ -54,15 +59,78 @@ const createTweetElement = function(tweet) {
  <span class="tweet-text">${tweet.content.text}</span>
  <footer>	
     <div>
-         <h6>${tweet.created_at}</h6> 
+    <h6>${moment(tweet['created_at']).fromNow()}</h6>
     </div>
     <div>
-         🏳️🔁💙
+    <i class="fas fa-flag"></i>
+    <i class="fas fa-retweet"></i>
+    <i class="fas fa-heart"></i>
        </div>    
  </footer>
 </article>`).addClass('tweet');
   // ...
   return $tweet;
 }
+ //function for load all tweets.
+function loadTweets() {
+  $.ajax({
+    url: '/tweets',
+    method: 'GET',
+    dataType: "json",
+    success: function (tweetsArray) {
+      //console.log('Success POST : ', tweetsArray);
+      renderTweets(tweetsArray);
+    }
+  });
+}
+// loadTweets();
+$(document).ready(function() {
+  $(function() {
+    //get the form.
+   const $form = $('#submit');
+    //get the session of article.
+    const $tweets = $('.container-tweets');
+    const $textarea = $('#tweet-area');
+    let $errSpan = $('.err-message');
+    let $errSht = $('.shrterr');
+    // Set up an event listener for the contact form.
+    $form.submit( function(event) {
+       $errSpan.slideUp(300);
+       $errSht.slideUp(300);
+      // Stop the browser from submitting the form.
+      event.preventDefault();
+      // if tweent is empty or more than 140 just display span with proper msg for 1500ms time.
+      var text = $textarea.val();
+      if(text === '' || text === null) {
+        $errSht.text("Say sumthing guyssss !!!!!!")
+        .slideToggle(400);
 
-renderTweets(data);
+      } else if(text.length > 140) {
+        $errSpan.text("Tweet length more than 140 characters")
+        .slideToggle(400);
+      } else {
+        // Submit the form using AJAX.
+        $.ajax({
+            type: 'POST',
+            url: '/tweets',
+            data: $form.serialize(),
+            success: function() {
+              loadTweets();
+            }
+        })
+        //empty the tweet textarea after tweet submits the post.
+        $textarea.val("");
+      }
+    });
+  });
+  //when we click compose button it slideDown newtweet and with the return value of slideToggle
+    // we are changing the toggle button appearance.
+  $(".toggle-btn").click(() => {
+    $(".new-tweet").slideToggle("slow");
+    $(".new-tweet textarea").focus();
+  });
+  //The new tweet area is hidden until called upon
+  $(".new-tweet").hide();
+  
+});
+ 
